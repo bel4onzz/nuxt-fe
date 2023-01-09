be9aee3261e3665d79bbb6bf5257a5b3efb6c6b4
Aleksandar <vasilevski.aleksandar@outlook.com>
2023-01-09 13:48:17 +0100

    [main]changes

--- pages/index.vue
+++ pages/index.vue
@@ -44,7 +44,7 @@ const posts = useNuxtApp().$apiFetch('/api/posts')
     <PostItem v-for="post in posts" :key="post.id" :post="post"></PostItem>
     <!-- <LazyApiPostItem v-for="post in api_posts" :key="post.id" :post="api_posts"></LazyApiPostItem> -->
     <div class="container mx-auto w-1/2 py-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        123 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
